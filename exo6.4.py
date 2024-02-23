21f51b01b4b4516030139ed36528cd7117ed5436
Mindsterz <43070071+Mindsterz@users.noreply.github.com>
2024-02-23 23:10:27 +0100

    Update exo6.4.py
    Ajout de commentaire pour la compréhension

--- exo6.4.py
+++ exo6.4.py
@@ -1,16 +1,16 @@
-def ordreCroissant(a,b) :
+def ordreCroissant(a,b) : #fonction permettant de remettre dans l'ordre les nombres input par l'utilisateur
     if a > b :
         return b,a
     
     if b > a :
         return a,b
 
-def sommeEntiers(a,b) :
+def sommeEntiers(a,b) : #fait la dite somme
 
     petit,grand = ordreCroissant(a,b)
     res = petit
 
-    for i in range(petit+1,grand+1,1) :
+    for i in range(petit+1,grand+1,1) : #parcours la liste en incrementant le plus petit nombre jusqu'au plus grand en faisant la somme à chaque fois 
 
         res = res + i
     
@@ -20,4 +20,4 @@ a = int(input("A ? "))
 b = int(input("B ? "))
 petit,grand = ordreCroissant(a,b)
 
-print("La somme des entiers de",petit,"à",grand,"est",sommeEntiers(a,b))
+print("La somme des entiers de",petit,"à",grand,"est",sommeEntiers(a,b)) #affiche le résultat
