1fff4d6d71d8745df7dcdb4ce4453df279a207b6
Altair <43070071+hugobder@users.noreply.github.com>
2024-09-02 17:24:58 +0200

    changement fonction ordre croissant

--- exo6.4.py
+++ exo6.4.py
@@ -1,16 +1,12 @@
-def ordreCroissant(a,b) : #fonction permettant de remettre dans l'ordre les nombres input par l'utilisateur
-    if a > b :
-        return b,a
-    
-    if b > a :
-        return a,b
+def ordreCroissant(a,b) :
+    return min(a,b), max(a,b)
 
-def sommeEntiers(a,b) : #fait la dite somme
+def sommeEntiers(a,b) :
 
     petit,grand = ordreCroissant(a,b)
     res = petit
 
-    for i in range(petit+1,grand+1,1) : #parcours la liste en incrementant le plus petit nombre jusqu'au plus grand en faisant la somme à chaque fois 
+    for i in range(petit+1,grand+1,1) :
 
         res = res + i
     
@@ -20,4 +16,4 @@ a = int(input("A ? "))
 b = int(input("B ? "))
 petit,grand = ordreCroissant(a,b)
 
-print("La somme des entiers de",petit,"à",grand,"est",sommeEntiers(a,b)) #affiche le résultat
+print("La somme des entiers de",petit,"à",grand,"est",sommeEntiers(a,b))
